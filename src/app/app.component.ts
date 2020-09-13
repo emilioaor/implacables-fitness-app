0251b05bc833b2a3fc725c978e5253176d5c27fb
Emilio Ochoa <eochoa@jacidi.com>
2020-09-13 09:31:09 -0400

    Notificaciones push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {
+  LocalNotificationsPlugin,
+  Plugins, PushNotification, PushNotificationActionPerformed, PushNotificationsPlugin,
+  PushNotificationToken
+} from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +16,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent implements OnInit {
 
+  pushNotifications: PushNotificationsPlugin;
+  localNotification: LocalNotificationsPlugin;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    const {PushNotifications} = Plugins;
+    const {LocalNotifications} = Plugins;
+
+    this.pushNotifications = PushNotifications;
+    this.localNotification = LocalNotifications;
+
     this.initializeApp();
   }
 
@@ -23,9 +37,61 @@ export class AppComponent implements OnInit {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.handleNotifications();
     });
   }
 
   ngOnInit() {
+  }
+
+  handleNotifications() {
+    this.pushNotifications.requestPermission().then( result => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        this.pushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    this.pushNotifications.addListener('registration',
+        (token: PushNotificationToken) => {
+          console.log('Registration success Firebase', token);
+        }
+    );
+
+    // Some issue with our setup and push will not work
+    this.pushNotifications.addListener('registrationError',
+        (error: any) => {
+          console.log(error);
+        }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    this.pushNotifications.addListener('pushNotificationReceived',
+        async (notification: PushNotification) => {
+          const not = await this.localNotification.schedule({
+            notifications: [
+              {
+                title: notification.title,
+                body: notification.body,
+                id: 1,
+                sound: null,
+                attachments: null,
+                actionTypeId: '',
+                extra: null
+              }
+            ]
+          });
+        }
+    );
+
+    // Method called when tapping on a notification
+    this.pushNotifications.addListener('pushNotificationActionPerformed',
+        (notification: PushNotificationActionPerformed) => {
+          alert('Push action performed: ' + JSON.stringify(notification));
+        }
+    );
   }
 }
